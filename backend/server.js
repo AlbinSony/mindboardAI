d9f4c278492974644b7f48b5cf9a395b3fc2038f
Albin Sony <albinsony2025@cs.sjcetpalai.ac.in>
2025-07-25 13:27:13 +0530

    initial setup and routed done

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import notesRoutes from './routes/notesRoutes.js'
+const app = express()
+
+app.use("/api/notes",notesRoutes)
+
+app.listen(5002,()=> {
+    console.log("Server Running on Port:5001");
+})
