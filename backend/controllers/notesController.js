4a820880c65d8b1a02c44e5bddd9523c9f4aecf0
Albin Sony <albinsony2025@cs.sjcetpalai.ac.in>
2025-07-25 13:29:47 +0530

    route update

--- backend/controllers/notesController.js
+++ backend/controllers/notesController.js
@@ -1,15 +1,15 @@
-export function getAllNotes (req,res) {
+export function getAllNotes(req,res) {
     res.send("You Just Fetched Notes")
 };
 
-export function createNote (req,res) {
+export function createNote(req,res) {
     res.status(201).json({message:"Note Created succesfully"});
 };
 
-export function updateNote (req,res) {
+export function updateNote(req,res) {
     res.status(200).json({message:"Note Updated succesfully"});
 }
 
-export function deleteNote (req,res) {
+export function deleteNote(req,res) {
     res.status(200).json({message:"Note Deleted succesfully"});
 }
