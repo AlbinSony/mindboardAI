9ca4a0aaa00a6bd821aa8d55b55a209cb30a8e9a
Albin Sony <albinsony2025@cs.sjcetpalai.ac.in>
2025-07-30 15:23:43 +0530

    schema added for users

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import notesRoutes from './routes/notesRoutes.js'
-const app = express()
+import { connectDB } from './config/db.js'
+import dotenv from "dotenv"
+dotenv.config()
 
+const app = express()
+const PORT = process.env.PORT || 5001
+connectDB()
 app.use("/api/notes",notesRoutes)
 
-app.listen(5002,()=> {
-    console.log("Server Running on Port:5001");
+app.listen(PORT,()=> {
+    console.log("Server Running on Port:",PORT);
 })
 
