6b38b274991faeb649ced40f22f4f685bf32cba5
Albin Sony <albinsony2025@cs.sjcetpalai.ac.in>
2025-08-05 12:10:06 +0530

    feat: add NoteCard component and integrate it into HomePage for displaying notes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import RateLimitedUI from "../components/RateLimitedUI";
+import NoteCard from "../components/NoteCard";
 import axios from "axios";
 import toast from "react-hot-toast";
 
@@ -37,6 +38,13 @@ const HomePage = () => {
       <Navbar />
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading && <div className="text-center text-primary py-10">Loading Notes...</div>}
+        {notes.length >0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note)=> (
+              <NoteCard key={note._id} note={note}/> 
+            ))}
+          </div>
+        )}
       </div>
       {isRateLimited && <RateLimitedUI/>}
     </div>
